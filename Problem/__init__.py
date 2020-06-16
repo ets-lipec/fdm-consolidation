22027bdc8beb2420a382de5d43c4cd8cd53af540
EthanCabon <ethan.cabon@grenoble-inp.org>
2020-06-16 16:06:18 +0200

    Create __init__.py

---
+++ Problem/__init__.py
@@ -0,0 +1,10 @@
+from .Deck import *
+from .Matrice_generator import *
+from .Boundary_conditions import *
+from .Initial_conditions import *
+from .Solve import *
+from .Draw_temperature import *
+from .Calculate_deformation import *
+from .Draw_deformation import *
+
+
