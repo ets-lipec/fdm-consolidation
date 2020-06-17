ac868f7caddcf5bd92e18b6b402edc105b4fff23
EthanCabon <ethan.cabon@grenoble-inp.org>
2020-06-17 15:11:26 +0200

    Update solution.py

--- Problem/Solve/solution.py
+++ Problem/Solve/solution.py
@@ -2,41 +2,41 @@ import numpy as np
 
 class Solution:
     
-    def _init(self, deck, matrice_generates, initial_conditions, boundary):
+    def __init__(self, deck, matrix_generation, initial_conditions, boundary_conditions):
         
         self.deck = deck
-        self.matrice_generates = matrice_generates
+        self.matrix_generation = matrix_generation
         self.initial_conditions = initial_conditions
-        self.boundary = boundary
+        self.boundary_conditions = boundary_conditions
         self.hot_bed()
         self.bed_cooling()
         self.cold_bed()
     
     def hot_bed(self):
         
-        self.nt = int(float(deck.doc["Simulation"]["time"])/matrice_generates.dt)
-        self.Ttot = initial_conditions.T
+        self.nt = int(float(self.deck.doc["Simulation"]["time"])/self.matrix_generation.dt)
+        self.Ttot = self.initial_conditions.T
         for t in range (self.nt):
-            initial_conditions.T = np.dot(matrice_generates.M,initial_conditions.T)+boundary.Vamb
-            self.Ttot = np.append(self.Ttot,initial_conditions.T,axis=1)
+            self.initial_conditions.T = np.dot(self.matrix_generation.M,self.initial_conditions.T)+self.boundary_conditions.Vamb
+            self.Ttot = np.append(self.Ttot,self.initial_conditions.T,axis=1)
     
     def bed_cooling(self):
         
-        Vcooling = float(deck.doc["Simulation"]["Vcooling"])
-        timecooling = (initial_conditions.Tbed-boundary.Troom)/Vcooling
-        self.nt1 = int(timecooling/matrice_generates.dt)
+        Vcooling = float(self.deck.doc["Simulation"]["Vcooling"])
+        timecooling = (self.initial_conditions.Tbed-self.boundary_conditions.Troom)/Vcooling
+        self.nt1 = int(timecooling/self.matrix_generation.dt)
         
-        boundary.Vamb[0]=-Vcooling*matrice_generates.dt
+        self.boundary_conditions.Vamb[0]=-Vcooling*self.matrix_generation.dt
         for t in range (self.nt1):
-            initial_conditions.T = np.dotmatrice_generates.(M,initial_conditions.T)+boundary.Vamb
-            self.Ttot = np.append(self.Ttot,initial_conditions.T,axis=1)
+            self.initial_conditions.T = np.dot(self.matrix_generation.M,self.initial_conditions.T)+self.boundary_conditions.Vamb
+            self.Ttot = np.append(self.Ttot,self.initial_conditions.T,axis=1)
     
     def cold_bed(self):
-        self.nt2 = int(float(deck.doc["Simulation"]["time2"])/matrice_generates.dt)
+        self.nt2 = int(float(self.deck.doc["Simulation"]["time2"])/self.matrix_generation.dt)
         
-        boundary.Vamb[0]=0
+        self.boundary_conditions.Vamb[0]=0
         for t in range (self.nt2):
-            initial_conditions.T = np.dot(matrice_generates.M,initial_conditions.T)+boundary.Vamb
-            self.Ttot = np.append(self.Ttot,initial_conditions.T,axis=1)
+            self.initial_conditions.T = np.dot(self.matrix_generation.M,self.initial_conditions.T)+self.boundary_conditions.Vamb
+            self.Ttot = np.append(self.Ttot,self.initial_conditions.T,axis=1)
         
         
