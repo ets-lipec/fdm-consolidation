d31160546e3a52c9ee9dc24a19594df4375e66ff
EthanCabon <ethan.cabon@grenoble-inp.org>
2020-06-17 15:10:27 +0200

    Update deformation_calculation.py

--- Problem/Calculate_deformation/deformation_calculation.py
+++ Problem/Calculate_deformation/deformation_calculation.py
@@ -1,12 +1,19 @@
 class Deformation_Calculation():
     
-    def _init_(self, deck, initial_conditions, solution):
+    def __init__(self, deck, matrix_generation, initial_conditions, solution):
         
         self.deck = deck
+        self.matrix_generation = matrix_generation
         self.initial_conditions = initial_conditions
         self.solution = solution
+        self.do_calculation()
         
     def do_calculation(self):
         
-        Tmoy = (solution.Ttot[0,:]+2*solution.Ttot[1,:]+2*solution.Ttot[2,:]+2*solution.Ttot[3,:]+solution.Ttot[4,:])/8 #Température moyenne par la méthode des trapèzes 
-        self.Eps = float(deck.doc["Materials"]["Mechanical"]["Coefficient of Thermal Expansion"])*(Tmoy-initial_conditions.Textrusion)
+        Tmoy=self.solution.Ttot[0,:]+self.solution.Ttot[self.matrix_generation.nx-1,:]
+        
+        for i in range (1,self.matrix_generation.nx-1):
+            Tmoy += 2*self.solution.Ttot[i,:]
+        
+        Tmoy /= 2*self.matrix_generation.nx-1
+        self.Eps = float(self.deck.doc["Materials"]["Mechanical"]["Coefficient of Thermal Expansion"])*(Tmoy-self.initial_conditions.Textrusion)
